957078449aae0f023a3337872594f554de9c8e46
thecraigus <53895450+thecraigus@users.noreply.github.com>
2021-11-19 14:14:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CAT_FACTS_URL = 'https://catfact.ninja/fact'
 
 app = Flask(__name__)
 
-#api = WebexTeamsAPI()
+api = WebexTeamsAPI()
 
 
 def get_catfact():
@@ -51,31 +51,64 @@ def webex_teams_webhook_events():
                    </html>
                 """.format(get_catfact()))
     elif request.method == 'POST':
+        json_data = request.json
+        print("\n")
+        print("WEBHOOK POST RECEIVED:")
+        print(json_data)
+        print("\n")
 
-        username = 'devnetuser'
-        password = 'RG!_Yw919_83'
-        vManage = 'sandbox-sdwan-1.cisco.com'
-        authurl = 'https://{}/j_security_check'.format(vManage)
-        authbody = {'j_username': f'{username}', 'j_password': f'{password}'}
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
-        url = f'https://{vManage}/dataservice/'
-        token_url = url + 'client/token'
+        # Create a Webhook object from the JSON data
+        webhook_obj = Webhook(json_data)
+        # Get the room details
+        room = api.rooms.get(webhook_obj.data.roomId)
+        # Get the message details
+        message = api.messages.get(webhook_obj.data.id)
+        # Get the sender's details
+        person = api.people.get(message.personId)
 
-        viptella = requests.session()
-        authenticate = viptella.post(url=authurl, data=authbody, verify=False)
+        print("NEW MESSAGE IN ROOM '{}'".format(room.title))
+        print("FROM '{}'".format(person.displayName))
+        print("MESSAGE '{}'\n".format(message.text))
 
-        login_token = viptella.get(url=token_url, verify=False)
+        # This is a VERY IMPORTANT loop prevention control step.
+        # If you respond to all messages...  You will respond to the messages
+        # that the bot posts and thereby create a loop condition.
+        me = api.people.me()
+        if message.personId == me.id:
+            # Message was sent by me (bot); do not respond.
+            return 'OK'
 
-        viptella.headers['X-XSRF-TOKEN'] = login_token.content
+        else:
+            # Message was sent by someone else; parse message and respond.
+            if "/CAT" in message.text:
+                username = 'devnetuser'
+                password = 'RG!_Yw919_83'
+                vManage = 'sandbox-sdwan-1.cisco.com'
+                authurl = 'https://{}/j_security_check'.format(vManage)
+                authbody = {'j_username': f'{username}', 'j_password': f'{password}'}
+                headers = {'content-type': 'application/json', 'accept': 'application/json'}
+                url = f'https://{vManage}/dataservice/'
+                token_url = url + 'client/token'
+
+                viptella = requests.session()
+                authenticate = viptella.post(url=authurl, data=authbody, verify=False)
+
+                login_token = viptella.get(url=token_url, verify=False)
+
+                viptella.headers['X-XSRF-TOKEN'] = login_token.content
 
 
-        getEvents = viptella.get(url=f"https://{vManage}:443/dataservice/device/monitor", verify=False).json()
+                getEvents = viptella.get(url=f"https://{vManage}:443/dataservice/device/monitor", verify=False).json()
 
-        deviceStatus = []
-        for device in getEvents['data']:
-            deviceStatus.append(device['host-name']+f' Status: {device["status"]}')
+                deviceStatus = []
+                for device in getEvents['data']:
+                    deviceStatus.append(device['host-name']+f' Status: {device["status"]}')
+                api.messages.create(room.id, text=str(deviceStatus))
+            return 'OK'
 
-        return str(deviceStatus)
+
+
+#         return str(deviceStatus)
 
 if __name__ == '__main__':
     # Start the Flask web server
